09fa99cc5def2032bb86d043898f539b61907b96
Mona Qiu <mq438@nyu.edu>
2017-03-28 21:40:22 -0400

    add img cache

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-03-28T22:45:25.998Z';
+var CACHE_NAME = 'cache-v2017-03-29T01:39:57.448Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
